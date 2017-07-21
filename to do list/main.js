bd3a82c48be3bb49699b94c39424ec5cbc102003
sameeransari81 <sameeransari8165@gmail.com>
2017-07-21 12:04:15 +0530

    beautified

--- to do list/main.js
+++ to do list/main.js
@@ -1,5 +1,24 @@
 var ngTodo = angular.module('ngTodo',[]);
 
 ngTodo.controller('mainController', function($scope) {
+	$scope.Todos=[{
+		'title' :'Do my Homework',
+		'details':'I must do my homework'
+	},
+	{
+		'title':'Acadview',
+		'details':'Acadview is damn awesome'
+	},
+	{
+		'title':'assignment',
+		'details':'i must do my assignment'
+	}]
+	$scope.addwork = function(work){
+		$scope.Todos.push(work);
+	}
+	$scope.deletework = function(work){
+		var index = $scope.Todos.indexOf(work);
+		$scope.Todos.splice(index , 1);
+	}
 	
 });
